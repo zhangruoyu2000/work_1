e23b17dfbff7047be2133066a8cb05a18e6299d6
Your Name <you@example.com>
2020-12-18 04:49:36 +0800

    功能5基本实现

--- serv/grade_actions.py
+++ serv/grade_actions.py
@@ -65,17 +65,49 @@ async def edit_grade_action(request):
     return web.HTTPFound(location="/grade")
 
 
-@web_routes.post('/action/grade/delete/{stu_sn}/{cou_sn}')
-def delete_grade_action(request):
-    stu_sn = request.match_info.get("stu_sn")
-    cou_sn = request.match_info.get("cou_sn")
-    if stu_sn is None or cou_sn is None:
-        return web.HTTPBadRequest(text="stu_sn, cou_sn, must be required")
+# @web_routes.post('/action/grade/delete/{stu_sn}/{cou_sn}')
+# def delete_grade_action(request):
+#     stu_sn = request.match_info.get("stu_sn")
+#     cou_sn = request.match_info.get("cou_sn")
+#     if stu_sn is None or cou_sn is None:
+#         return web.HTTPBadRequest(text="stu_sn, cou_sn, must be required")
 
-    with db_block() as db:
-        db.execute("""
-        DELETE FROM course_grade
-            WHERE stu_sn = %(stu_sn)s AND cou_sn = %(cou_sn)s
-        """, dict(stu_sn=stu_sn, cou_sn=cou_sn))
+#     with db_block() as db:
+#         db.execute("""
+#         DELETE FROM course_grade
+#             WHERE stu_sn = %(stu_sn)s AND cou_sn = %(cou_sn)s
+#         """, dict(stu_sn=stu_sn, cou_sn=cou_sn))
 
-    return web.HTTPFound(location="/grade")
+#     return web.HTTPFound(location="/grade")
+
+# @web_routes.post('/action/grade/select/{stu_sn}')
+# async def select_action_grade(request):
+#     # params = await request.post()
+#     # stu_sn = params.get("stu_sn")
+#     stu_sn = request.match_info.get("stu_sn")
+
+#     if stu_sn is None :
+#         return web.HTTPBadRequest(text="stu_sn, cou_sn, grade must be required")
+
+#     try:
+#         stu_sn = int(stu_sn)
+#     except ValueError:
+#         return web.HTTPBadRequest(text="invalid value")
+
+#     try:
+#         with db_block() as db:
+#             db.execute("""
+#             SELECT stu_sn, cou_sn, grade
+#             FROM course_grade
+#             WHERE stu_sn = %(stu_sn)s
+#             """, dict(stu_sn=stu_sn))
+#     except psycopg2.errors.UniqueViolation:
+#         query = urlencode({
+#             "message": "已经添加该学生的课程成绩",
+#             "return": "/grade"
+#         })
+#         return web.HTTPFound(location=f"/error?{query}")
+#     except psycopg2.errors.ForeignKeyViolation as ex:
+#         return web.HTTPBadRequest(text=f"无此学生或课程: {ex}")
+
+#     return web.HTTPFound(location="/grade")
--- serv/grade_views.py
+++ serv/grade_views.py
@@ -28,9 +28,22 @@ async def view_list_grades(request):
 
         items = list(db)
 
+        db.execute("""
+        SELECT g.stu_sn, g.cou_sn, 
+            s.name as stu_name, 
+            c.name as cou_name, 
+            g.grade 
+        FROM course_grade as g
+            INNER JOIN student as s ON g.stu_sn = s.sn
+            INNER JOIN course as c  ON g.cou_sn = c.sn
+        ORDER BY stu_sn, cou_sn;
+        """)
+        grades = db.fetch_first()
+
     return render_html(request, 'grade_list.html',
                        students=students,
                        courses=courses,
+                       grades=grades,
                        items=items)
 
 
@@ -83,3 +96,39 @@ def grade_deletion_dialog(request):
         return web.HTTPNotFound(text=f"no such grade: stu_sn={stu_sn}, cou_sn={cou_sn}")
 
     return render_html(request, 'grade_dialog_deletion.html', record=record)
+
+
+@web_routes.get("/grade/select/{stu_sn}/{cou_sn}")
+def grade_select_dialog(request):
+    stu_sn = request.match_info.get("stu_sn")
+    cou_sn = request.match_info.get("cou_sn")
+
+    if stu_sn is None :
+        return web.HTTPBadRequest(text="stu_sn, cou_sn, must be required")
+
+    with db_block() as db:
+        db.execute("""
+        SELECT g.stu_sn, g.cou_sn, 
+            s.name as stu_name, 
+            c.name as cou_name, 
+            g.grade 
+        FROM course_grade as g
+            INNER JOIN student as s ON g.stu_sn = s.sn
+            INNER JOIN course as c  ON g.cou_sn = c.sn
+        WHERE stu_sn = %(stu_sn)s ;
+        """, dict(stu_sn=stu_sn))
+
+        records = list(db)
+
+        db.execute("""
+        SELECT sn AS stu_sn, name as stu_name FROM student 
+        WHERE sn = %(stu_sn)s;
+        """, dict(stu_sn=stu_sn))
+
+        students = db.fetch_first()
+
+
+    if records is None:
+        return web.HTTPNotFound(text=f"no such grade: stu_sn={stu_sn}")
+
+    return render_html(request, 'grade_dialog_select.html', records=records, students=students)
